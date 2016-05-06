f3fe12ede77ff899bcdd4f5513b3175495be7556
sunny <sunnykraditya@gmail.com>
2016-05-06 16:18:22 +0530

    Added methods for tracking dowloaded

---
+++ app/src/main/java/com/one/assignment/listeners/DownloaderListener.java
@@ -0,0 +1,16 @@
+package com.one.assignment.listeners;
+
+import com.one.assignment.models.MovieFeed;
+
+/**
+ * Created by Sunny on 06-05-2016.
+ */
+public interface DownloaderListener {
+
+    void onFinished(MovieFeed feed);
+
+    void onStarted();
+
+    void onFailed();
+
+}
